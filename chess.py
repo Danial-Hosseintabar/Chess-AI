f1052a87738147b3b33e5345d8b0da6ba792bd7a
Danial-Hosseintabar <danial.ht10@gmail.com>
2023-09-19 18:22:09 +0330

    get moves in chess.py

--- chess.py
+++ chess.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+INF = 100
+
 board = []
 
 class Piece(Enum):
@@ -32,7 +34,7 @@ for row_number in range(2, 6):
 
 for row_number in [1, 6]:
     for j in range(0, 8):
-        board[row_number].append(0)
+        board[row_number].append((Piece.PAWN, Player.BLACK if row_number == 1 else Player.WHITE ))
 
 board[0] = [(Piece.ROOK, Player.BLACK),
             (Piece.KNIGHT, Player.BLACK),
@@ -51,3 +53,115 @@ board[7] = [(Piece.ROOK, Player.WHITE),
             (Piece.BISHOP, Player.WHITE),
             (Piece.KNIGHT, Player.WHITE),
             (Piece.ROOK, Player.WHITE)]
+
+
+def valid_pos(row, column, max_distance):
+    return 0 <= row and row < 8 and column < 8 and column >= 0
+
+def get_rook_moves(row, column, max_distance):
+    ret = []
+    delta = 1
+    while(delta <= max_distance and valid_pos(row + delta, column) and board[row+delta][column][0] == Piece.EMPTY):
+        ret.append((row + delta, column))
+        delta += 1
+
+    delta = 1
+    while(delta <= max_distance and valid_pos(row - delta, column) and board[row - delta][column][0] == Piece.EMPTY):
+        ret.append((row - delta, column))
+        delta += 1
+    
+    delta = 1
+    while(delta <= max_distance and valid_pos(row, column + delta) and board[row][column + delta][0] == Piece.EMPTY):
+        ret.append((row, column + delta))
+        delta += 1
+    
+    delta = 1
+    while(delta <= max_distance and valid_pos(row, column - delta) and board[row][column - delta][0] == Piece.EMPTY):
+        ret.append((row, column - delta))
+        delta += 1
+    
+    return ret
+
+def get_bishop_moves(row, column, max_distance):
+    ret = []
+
+    delta = 1
+    while(delta <= max_distance and valid_pos(row + delta, column + delta) and board[row + delta][column + delta][0] == Piece.EMPTY):
+        ret.append((row + delta, column + delta))
+        delta += 1
+
+    delta = 1
+    while(delta <= max_distance and valid_pos(row - delta, column - delta) and board[row - delta][column - delta][0] == Piece.EMPTY):
+        ret.append((row - delta, column - delta))
+        delta += 1
+
+    delta = 1
+    while(delta <= max_distance and valid_pos(row + delta, column - delta) and board[row + delta][column - delta][0] == Piece.EMPTY):
+        ret.append((row + delta, column - delta))
+        delta += 1
+
+    delta = 1
+    while(delta <= max_distance and valid_pos(row - delta, column + delta) and board[row - delta][column + delta][0] == Piece.EMPTY):
+        ret.append((row - delta, column + delta))
+        delta += 1
+
+    return ret
+
+def get_moves(row, column):
+    ret = []
+
+    # pawn
+
+    if(board[row][column][0] == Piece.PAWN):
+        next_row = row + 1 if board[row][column][1] == Player.BLACK else row - 1
+        next_next_row = row + 2 if board[row][column][1] == Player.BLACK else row - 2
+        
+        if not valid_pos(next_row, column):
+            return ret
+        elif board[next_row][column][0] == Piece.EMPTY:
+            ret.append((next_row, column))
+        
+        if not valid_pos(next_next_row, column):
+            return ret
+        elif board[next_next_row][column][0] == Piece.EMPTY:
+            ret.append((next_next_row, column))
+
+        return ret
+
+
+    # rook
+
+    if(board[row][column][0] == Piece.ROOK):
+        for cell in get_rook_moves(row, column, INF):
+            ret.append(cell)
+        return ret
+    
+    # bishop
+
+    if(board[row][column][0] == Piece.BISHOP):
+        for cell in get_bishop_moves(row, column, INF):
+            ret.append(cell)
+        return ret
+
+    # queen
+
+    if(board[row][column][0] == Piece.QUEEN):
+        for cell in get_bishop_moves(row, column, INF):
+            ret.append(cell)
+        
+        for cell in get_rook_moves(row, column, INF):
+            ret.append(cell)
+        
+        return ret
+    
+    # king
+
+    if(board[row][column][0] == Piece.KING):
+        for cell in get_bishop_moves(row, column, 1):
+            ret.append(cell)
+        
+        for cell in get_rook_moves(row, column, 1):
+            ret.append(cell)
+        
+        return ret
+
